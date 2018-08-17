5c43c6a8bf4afd4eb2f7fa13d29b6263a62fd080
TomRichards14 <2richt38@solent.ac.uk>
2018-08-17 18:58:58 +0100

    Player movement

---
+++ Building Swing/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private float PlayerSpeed = 0.1f;
+    private float PlayerJumpMultiplier = 500.0f;
+
+    private bool IsPlayerGrounded;
+
+	// Use this for initialization
+	void Start ()
+    {
+        IsPlayerGrounded = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        PlayerMovement();
+        Debug.Log(IsPlayerGrounded);
+	}
+
+    void PlayerMovement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * PlayerSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * PlayerSpeed);
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Space)) && (IsPlayerGrounded == true))
+        {
+            this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * PlayerJumpMultiplier);
+        }
+    }
+
+    private void OnCollisionEnter(Collision OtherCollider)
+    {
+        if ((OtherCollider.gameObject.tag == "Building") || (OtherCollider.gameObject.tag == "Rope"))
+        {
+            IsPlayerGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision OtherCollider)
+    {
+        if ((OtherCollider.gameObject.tag == "Building") || (OtherCollider.gameObject.tag == "Rope"))
+        {
+            IsPlayerGrounded = false;
+        }
+    }
+}
